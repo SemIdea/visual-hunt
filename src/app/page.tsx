0546538d3f058419365dad205f23c756efe2ace9
SemIdeia <codorkman@gmail.com>
2025-05-09 11:10:22 -0300

    Adding slogan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Slogan from "@/components/slogan";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Slogan />
     </div>
   );
 }
---
+++ src/components/slogan/index.tsx
@@ -0,0 +1,15 @@
+const Slogan = () => {
+  return (
+    <section className="my-16 pt-12 text-center flex flex-col gap-6">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-primary">
+        Visual Hunt
+      </h1>
+      <p className="text-lg md:text-xl max-w-2xl mx-auto text-gray-400">
+        Upload an image or video, select a specific region, and discover
+        visually similar content across the web.
+      </p>
+    </section>
+  );
+};
+
+export default Slogan;
