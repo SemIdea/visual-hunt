3d76b61cc6c878eac2bac254a0640c1bc8187042
SemIdea <codorkman@gmail.com>
2025-10-01 15:44:50 -0300

    fix: fixing index

--- src/app/(home)/_components/upload/index.tsx
+++ src/app/(home)/_components/upload/index.tsx
@@ -6,14 +6,13 @@ import UrlTab from "./url";
 const UploadSectionCopy = () => {
   return (
     <div className="border-1 border-zinc-800 rounded-t-2xl rounded-b-md max-w-3xl w-full">
-      <Tabs radius="sm" fullWidth>
-        {/* <Tab key="upload" title="Upload File">
+      {/* <Tabs radius="sm" fullWidth>
+        <Tab key="upload" title="Upload File">
           <UploadTab />
-        </Tab> */}
-        <Tab key="url" title="URL">
-          <UrlTab />
         </Tab>
-      </Tabs>
+        <Tab key="url" title="URL"></Tab>
+      </Tabs> */}
+      <UrlTab />
     </div>
   );
 };
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,13 @@
 import Features from "@/app/(home)/_components/features";
 import Slogan from "@/app/(home)/_components/slogan";
-import Upload from "@/app/(home)/_components/upload/intex";
+import Upload from "@/app/(home)/_components/upload";
 
 export default function Home() {
   return (
-      <div className="flex flex-col items-center">
-        <Slogan />
-        <Upload />
-        <Features />
-      </div>
+    <div className="flex flex-col items-center">
+      <Slogan />
+      <Upload />
+      <Features />
+    </div>
   );
 }
