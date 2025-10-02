d2814ce4bf867647caed282bc8b6931c028ac682
SemIdea <codorkman@gmail.com>
2025-10-01 22:23:52 -0300

    feat: passing user in protected context

--- src/server/createContex.ts
+++ src/server/createContex.ts
@@ -1,23 +1,48 @@
+import { getServerSession } from "next-auth";
 import { helpers, IHelpers } from "./container/helpers";
 import { IRepositories, repositories } from "./container/repositories";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { UserEntity } from "./entities/user/entity";
 
 type IBaseContextDTO = {
   helpers: IHelpers;
   repositories: IRepositories;
 };
 
-type IAPIContextDTO = IBaseContextDTO & {};
+type IAPIContextDTO = IBaseContextDTO & {
+  userId?: string;
+};
 
-const createTRPCContext = () => {
+type IProtectedAPIContextDTO = IBaseContextDTO & {
+  userId: string;
+};
+
+const createTRPCContext = async (): Promise<IAPIContextDTO> => {
   const ctx: IAPIContextDTO = {
     helpers,
     repositories,
   };
 
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) return ctx;
+
+  const user = await UserEntity.readByEmail({
+    email: session.user.email!,
+    repositories: {
+      ...ctx.repositories,
+      database: ctx.repositories.user,
+    },
+  });
+
+  if (!user) return ctx;
+
+  ctx.userId = user.id;
+  
   return ctx;
 };
 
 type Context = Awaited<ReturnType<typeof createTRPCContext>>;
 
 export { createTRPCContext };
-export type { Context, IAPIContextDTO };
+export type { Context, IAPIContextDTO, IProtectedAPIContextDTO };
