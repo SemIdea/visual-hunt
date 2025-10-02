2b1127994fb6ba14895be5e0674dee7136826f84
SemIdea <codorkman@gmail.com>
2025-10-02 17:26:08 -0300

    refactor: using purple as a primary color

--- src/app/(home)/_components/upload/url.tsx
+++ src/app/(home)/_components/upload/url.tsx
@@ -2,6 +2,8 @@
 
 import { trpc } from "@/app/_trpc/client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { File } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
@@ -35,25 +37,24 @@ const UrlTab = () => {
   return (
     <div className="p-6">
       <div className="grid w-full gap-1.5">
-        <label htmlFor="url" className="text-sm font-medium">
-          Image URL
-        </label>
-        <div className="flex gap-2">
-          <input
-            id="url"
-            placeholder="https://example.com/image.jpg"
-            className="flex h-10 w-full rounded-md border border-zinc-800 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-            onChange={(e) => setImageUrl(e.target.value)}
-          />
-          <Button
-            color="primary"
-            className="text-zinc-900"
-            // onPress={handleSubmit}
-          >
-            <File className="h-7 w-7" />
-            Fetch
-          </Button>
+        <div className="grid w-full items-center gap-3">
+          <Label htmlFor="url">Image URL</Label>
+          <div className="flex gap-3">
+            <Input
+              id="url"
+              placeholder="https://example.com/image.jpg"
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+            <Button
+              color="primary"
+              className="cursor-pointer"
+              onClick={handleSubmit}
+            >
+              Search
+            </Button>
+          </div>
         </div>
+
         <p className="text-xs text-muted-foreground">
           Enter the URL of an image to search
         </p>
