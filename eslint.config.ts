f47dffe387f3283cc10aed1ed7d73024dc32a1f4
SemIdea <codorkman@gmail.com>
2025-10-01 22:21:40 -0300

    chore: implementing eslint next js plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
 
 export default defineConfig([
-  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
-
   {
+    extends: ["plugin:@next/next/recommended"],
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
