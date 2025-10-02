ecf2b839a7565fb02059eebb0d0b2cfee07068aa
SemIdea <codorkman@gmail.com>
2025-10-01 22:24:20 -0300

    chore: creating protectedProcedure

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -1,8 +1,24 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { Context } from "./createContex";
 
 const t = initTRPC.context<Context>().create();
 
 const publicProcedure = t.procedure;
 
-export { t, publicProcedure };
+const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.userId) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "User is not authenticated",
+    });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      userId: ctx.userId
+    },
+  });
+});
+
+export { t, publicProcedure, protectedProcedure };
