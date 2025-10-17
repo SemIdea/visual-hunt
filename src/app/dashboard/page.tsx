ea70090081e42af3b00e1a12fa800fb26ece5017
SemIdea <codorkman@gmail.com>
2025-10-17 16:49:45 -0300

    feat: implement session authentication and redirect to login if not authenticated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,16 @@
+import { redirect } from "next/navigation";
+import { auth } from "../api/auth/[...nextauth]/auth";
 import UserInfo from "./_components/header";
 import PlanUsage from "./_components/planUsage";
 import DashboardNavigationTabs from "./_components/tabs";
 
 const Page = async () => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
   return (
     <div className="min-h-screen bg-background">
       <div className="mx-auto max-w-6xl space-y-8">
